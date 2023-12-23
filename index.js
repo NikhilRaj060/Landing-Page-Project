f7df5191ef9cb2c8340cbbedc031374c2b1a000b
NikhilRaj060 <nikhilraj2061@gmail.com>
2023-12-23 20:28:42 +0530

    Cuvette assignment.

---
+++ index.js
@@ -0,0 +1,9 @@
+function register() {
+  document.getElementById("email").style.display = "none";
+  document.getElementById("button").style.display = "none";
+  let text = "Thank You!";
+  document.getElementById("thank_you_message").innerHTML = text;
+  document.getElementById("warn").style.padding = "5px 20px 20px 5px ";
+  document.getElementById("warn").style.justifyContent = "flex-start";
+  document.getElementById("warn").style.alignItems = "flex-start";
+}
